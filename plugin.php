9ff2ef744ec54d35b151086f21d8f19bfba059f3
wokamoto <wokamoto1973@gmail.com>
2013-05-14 15:06:05 +0900

    日本語リソース追加

--- plugin.php
+++ plugin.php
@@ -3,16 +3,38 @@
 Plugin Name: StaticPress
 Author: wokamoto 
 Plugin URI: http://www.digitalcube.jp/
-Description: 静的書き出しプラグイン
+Description: Transform your WordPress into static websites and blogs.
 Version: 0.3.3
 Author URI: http://www.digitalcube.jp/
+Text Domain: static-press
 Domain Path: /languages
-Text Domain: 
+
+License:
+ Released under the GPL license
+  http://www.gnu.org/copyleft/gpl.html
+
+  Copyright 2012 (email : wokamoto1973@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 if (!class_exists('static_static_admin'))
 	require_once(dirname(__FILE__).'/includes/class-admin-menu.php');
 if (!class_exists('static_static'))
 	require_once(dirname(__FILE__).'/includes/class-static_static.php');
+
+load_plugin_textdomain(static_static_admin::TEXT_DOMAIN, false, dirname(plugin_basename(__FILE__)) . '/languages/');
 
 $static_admin = new static_static_admin(plugin_basename(__FILE__));
 $static = new static_static(
---
+++ uninstall.php
@@ -0,0 +1,18 @@
+<?php
+
+if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+	exit();
+
+
+if (!class_exists('static_static_admin'))
+	require_once(dirname(__FILE__).'/includes/class-admin-menu.php');
+
+delete_option(static_static_admin::OPTION_STATIC_URL);
+delete_option(static_static_admin::OPTION_STATIC_DIR);
+delete_option(static_static_admin::OPTION_STATIC_BASIC);
+
+global $wpdb;
+
+$url_table = $wpdb->prefix.'urls';
+if ($wpdb->get_var("show tables like '{$url_table}'") != $url_table)
+	$wpdb->query("DROP TABLE `{$url_table}`");
